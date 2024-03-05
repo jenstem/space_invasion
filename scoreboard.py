8cc1629faaf59d41e48ad577336588f0e7f099f3
Jen <124745888+jenstem@users.noreply.github.com>
2024-03-05 16:51:33 -0500

    Add high score + key press functionality

--- scoreboard.py
+++ scoreboard.py
@@ -24,3 +24,27 @@ class Scoreboard:
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
+
+    def prep_high_score(self):
+        # Show the high score
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"High Score: {high_score:,}"
+        self.high_score_image = self.font.render(high_score_str, True,
+            self.text_color, self.settings.bg_color)
+
+        # Center the high score at the top of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def check_high_score(self):
+        # Check to see if there's a new high score
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+
+    def show_score(self):
+        # Show score on screen
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.ships.draw(self.screen)
--- space_invasion.py
+++ space_invasion.py
@@ -22,12 +22,54 @@ class SpaceInvasion:
         self.sb = Scoreboard(self)
         self.ship = Ship(self)
 
-        # Reset the game statistics.
-        self.stats.reset_stats()
-        self.sb.prep_ships()
+        # Start Space Invasion in an inactive state
+        self.game_active = False
 
-        # Hide the mouse cursor
-        pygame.mouse.set_visible(False)
+    def run_game(self):
+        while True:
+            self._check_events()
+
+            if self.game_active:
+                self.ship.update()
+
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        # Respond to keypresses and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_keydown_events(self, event):
+        #Respond to key presses
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        # Respond to key releases
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+            # Reset the game statistics.
+            self.stats.reset_stats()
+            self.sb.prep_ships()
+
+            # Hide the mouse cursor
+            pygame.mouse.set_visible(False)
 
     def _ship_hit(self):
         #Respond to the ship being hit by an alien
