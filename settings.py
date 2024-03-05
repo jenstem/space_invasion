2cfec869e18dc3b6607e29d983cde3793b60cdc6
Jen <124745888+jenstem@users.noreply.github.com>
2024-03-05 16:34:52 -0500

    Add game stats functionality

--- settings.py
+++ settings.py
@@ -5,3 +5,5 @@ class Settings:
         self.screen_height = 800
         # Black background
         self.bg_color = (0, 0, 0)
+        # Limit the number of ships a player starts with
+        self.ship_limit = 3
--- space_invasion.py
+++ space_invasion.py
@@ -13,6 +13,17 @@ class SpaceInvasion:
 
         pygame.display.set_caption("Space Invasion")
 
+        # Reset the game statistics.
+        self.stats.reset_stats()
+        self.sb.prep_ships()
+
+    def _ship_hit(self):
+        #Respond to the ship being hit by an alien
+        if self.stats.ships_left > 0:
+        # Decrement ships_left and update scoreboard
+            self.stats.ships_left -= 1
+            self.sb.prep_ships()
+
 if __name__ == '__main__':
     ai = SpaceInvasion()
     ai.run_game()
