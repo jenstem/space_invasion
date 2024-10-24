81300402a537c059b95ac7b5bfe4900b2db23f7f
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-24 15:23:56 -0400

    Update docstring game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -1,14 +1,21 @@
 class GameStats():
+    """
+    A class to track statistics for the game.
+    """
 
     def __init__(self, ai_game):
-        # Initialize stats
+        """
+        Initialize statistics.
+        """
         self.settings = ai_game.settings
         self.reset_stats()
         # High score should never be reset
         self.high_score = 0
 
     def reset_stats(self):
-        # Initialize statistics that can change during the game
+        """
+        Reset statistics that can change during the game.
+        """
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
