b05917644a1872082d1f6346f73933362a90a8f6
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-24 15:22:29 -0400

    Update docstring in button.py

--- button.py
+++ button.py
@@ -1,8 +1,14 @@
 import pygame.font
 
 class Button:
+    """
+    A class to create a button for the game interface.
+    """
 
     def __init__(self, ai_game, msg):
+        """
+        Initialize button attributes.
+        """
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
 
@@ -20,13 +26,17 @@ class Button:
         self._prep_msg(msg)
 
     def _prep_msg(self, msg):
-        # Turn msg into a rendered image and center text on the button
+        """
+        Turn msg into a rendered image and center text on the button
+        """
         self.msg_image = self.font.render(msg, True, self.text_color,
             self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
 
     def draw_button(self):
-        # Draw blank button and then draw message
+        """
+        Draw blank button and then draw message
+        """
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
