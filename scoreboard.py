13d644c7449c363a3831482a3b43dd28ea0f381e
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-24 15:27:12 -0400

    Update docstring in scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,12 +1,16 @@
 import pygame.font
 from pygame.sprite import Group
-
 from ship import Ship
 
 class Scoreboard:
-
+    """
+    A class to report scoring information.
+    """
+    
     def __init__(self, ai_game):
-        # Initialize score-keeping attributes
+        """
+        Initialize score-keeping attributes.
+        """
         self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
@@ -24,7 +28,9 @@ class Scoreboard:
         self.prep_ships()
 
     def prep_score(self):
-        # Score image
+        """
+        Prepare the score image
+        """
         rounded_score = round(self.stats.score, -1)
         score_str = f"Score: {rounded_score:,}"
         self.score_image = self.font.render(score_str, True, self.text_color,
@@ -36,7 +42,9 @@ class Scoreboard:
         self.score_rect.top = 20
 
     def prep_high_score(self):
-        # Show the high score
+        """
+        Prepare the high score image.
+        """
         high_score = round(self.stats.high_score, -1)
         high_score_str = f"High Score: {high_score:,}"
         self.high_score_image = self.font.render(high_score_str, True,
@@ -48,13 +56,17 @@ class Scoreboard:
         self.high_score_rect.top = self.score_rect.top
 
     def check_high_score(self):
-        # Check to see if there's a new high score
+        """
+        Check to see if there's a new high score
+        """
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
 
     def prep_level(self):
-        # Turn the level into a rendered image
+        """
+        Prepare the level into a rendered image.
+        """
         level_str = f"Level: {self.stats.level}"
         self.level_image = self.font.render(level_str, True, self.text_color,
             self.settings.bg_color)
@@ -65,7 +77,9 @@ class Scoreboard:
         self.level_rect.top = self.score_rect.bottom + 10
 
     def prep_ships(self):
-        # Show how many ships are left
+        """
+        Prepare the ships image.
+        """
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
             ship = Ship(self.ai_game)
@@ -74,7 +88,9 @@ class Scoreboard:
             self.ships.add(ship)
 
     def show_score(self):
-        # Show score on screen
+        """
+        Display score on the screen.
+        """
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
