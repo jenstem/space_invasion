7e2f85e886cfaeb85ce8d56369bdac3085391092
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-24 15:18:44 -0400

    Update docstring in alien.py

--- alien.py
+++ alien.py
@@ -2,8 +2,14 @@ import pygame
 from pygame.sprite import Sprite
 
 class Alien(Sprite):
+    """
+    A class to represent a single alient in the fleet.
+    """
 
     def __init__(self, ai_game):
+        """
+        Initialize the alient and set its starting position.
+        """
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
@@ -17,12 +23,16 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
 
     def check_edges(self):
-        # Return True if alien is at edge of screen
+        """
+        Return True if the alien is at the edge of the screen.
+        """
         screen_rect = self.screen.get_rect()
         return(self.rect.right >= screen_rect.right or self.rect.left <= 0)
 
 
     def update(self):
-        # Move the alien right or left
+        """
+        Move the alien to the right or to the left.
+        """
         self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
